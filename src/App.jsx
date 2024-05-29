788e050bb53cd2cb537c6730748bb4f8c3f279ca
rasyidf <ufarasfa@gmail.com>
2024-05-30 01:52:56 +0700

    initial repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@stradivary/common';
+import './App.css';
+
+function App() { 
+
+  return (
+    <>
+      <Button>
+        HEHE
+      </Button>
+    </>
+  );
+}
+
+export default App;
